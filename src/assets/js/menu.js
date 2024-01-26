aa79bee1c2abf1b60241f1ad29d3dc8b3323f0ac
git config --global user.email atrani@naver.com <atrani@naver.com>
2024-01-26 16:01:33 +0900

    헤더완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,19 @@
 export function menu(){
-    console.log(menu)
+    const headerToggle = document.getElementById("headerToggle");   //변수지정 id
+    const headerNav = document.querySelector(".header__nav");   //변수지정 class
+
+    if (headerToggle) { //언제나무조건항상실행되는게아니라 headerToggle이 있을때만 실행해라
+        headerToggle.addEventListener("click", () => {  //click하면
+        headerNav.classList.toggle("show");
+        //console.log(headerToggle.getAttribute("aria-expanded"))   //속성값 가져오기
+
+        
+        headerToggle.getAttribute("aria-expanded") === "true"   //ifelse문
+        ? headerToggle.setAttribute("aria-expanded", "false")   //true가 맞으면?
+        : headerToggle.setAttribute("aria-expanded", "true");   //true가 틀리면:
+
+
+        });
+    }
+
 }
